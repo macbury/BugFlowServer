64901d32db1bf096f3ea93a87b39270d89258b1d
macbury <macbury@gmail.com>
2012-03-12 12:56:26 +0100

    fix

--- worker/worker.rb
+++ worker/worker.rb
@@ -11,11 +11,10 @@ class Worker
     
     log "Connecting"
     @connection = AMQP.connect :user => "guest", :pass => "guest", :vhost => "/"
-    log "Adding channel"
+    log "Connecting to channel"
     @channel = AMQP::Channel.new(@connection, :auto_recovery => true)
     @channel.prefetch(10)
 
-    log "Binding"
     bind
     
     log "Boot successful"
@@ -31,10 +30,17 @@ class Worker
     puts msg
   end
   
-  
   def bind
+    log "Starting queue"
     @channel.queue("requests.ruby", :durable => true, :auto_delete => false).subscribe(:ack => true) do |metadata, payload|
-      log "recived!"
+      request = YAML.load(payload)
+      log "metadata: "
+      log metadata.inspect
+
+      log "Payload"
+      log request.inspect
+
+      EM.next_tick { request }
       metadata.ack
     end
   end
